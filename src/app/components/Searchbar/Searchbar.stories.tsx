68e7d3a4341254f013df1ffaf554350d72c33a02
dimas1976 <kogan.dmitriy@gmail.com>
2021-09-09 12:06:26 +0200

    ✨ Add story for searchbar component

---
+++ src/app/components/Searchbar/Searchbar.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Searchbar from './Searchbar';
+
+export default {
+  title: 'Component/Searchbar',
+  component: Searchbar,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const AppSearchbar = (): JSX.Element => (
+  <Searchbar
+    placeholder="Suche nach Name oder Stadt"
+    value=""
+    onValueChange={() => console.log()}
+  />
+);
