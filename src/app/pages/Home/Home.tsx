d2f98b2b6cdbb594e0ef43a721e48b028bc9b00b
dimas1976 <kogan.dmitriy@gmail.com>
2021-09-10 14:11:48 +0200

    ✨ Create home page component

---
+++ src/app/pages/Home/Home.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Searchbar from '../../components/Searchbar/Searchbar';
+import Navigation from '../../components/navigation/Navigation';
+import styles from './Home.module.css';
+
+export default function Home(): JSX.Element {
+  return (
+    <div className={styles.page}>
+      <header className={styles.page__header}>
+        <Header />
+      </header>
+      <div className={styles.page__searchbar}>
+        <Searchbar value="" onValueChange={(value) => console.log(value)} />
+      </div>
+      <section className={styles.page__section}></section>
+      <nav className={styles.page__navigation}>
+        <Navigation activeLink="home" className={styles.navigation} />
+      </nav>
+    </div>
+  );
+}
