c23c2349b2d2f2e199cd8835dde405e8e29b0f91
dimas1976 <kogan.dmitriy@gmail.com>
2021-09-11 14:17:45 +0200

    ✨ Set input field conrolled

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
 import Searchbar from '../../components/Searchbar/Searchbar';
 import Navigation from '../../components/navigation/Navigation';
 import styles from './Home.module.css';
 
 export default function Home(): JSX.Element {
+  const [searchValue, setSearchValue] = useState('');
   return (
     <div className={styles.page}>
       <header className={styles.page__header}>
@@ -12,9 +13,10 @@ export default function Home(): JSX.Element {
       </header>
       <div className={styles.page__searchbar}>
         <Searchbar
-          value=""
+          value={searchValue}
           placeholder={'Suche nach Name oder Stadt'}
-          onValueChange={(value) => console.log(value)}
+          onValueChange={(value) => setSearchValue(value)}
+          handleSubmit={(event) => event.preventDefault()}
         />
       </div>
       <section className={styles.page__section}></section>
