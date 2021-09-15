3bf59248803002f9b71afa8fd084eeb5967ddd00
dimas1976 <kogan.dmitriy@gmail.com>
2021-09-15 11:19:35 +0200

    ⚰️ remove unused code

--- src/app/components/ActionButton/ActionButton.tsx
+++ src/app/components/ActionButton/ActionButton.tsx
@@ -5,16 +5,14 @@ import styles from './ActionButton.module.css';
 type ActionButtonProps = {
   children: ReactNode;
   onClickHandle?: () => void;
-  className?: string;
 };
 
 export default function ActionButton({
   children,
   onClickHandle,
-  className,
 }: ActionButtonProps): JSX.Element {
   return (
-    <button className={`${className} ${styles.button}`} onClick={onClickHandle}>
+    <button className={styles.button} onClick={onClickHandle}>
       {children}
     </button>
   );
