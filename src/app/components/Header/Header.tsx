12a47134917604df8ac36ed3dcb93b1b33cf57b9
dimas1976 <kogan.dmitriy@gmail.com>
2021-09-17 16:30:16 +0200

    🔥 Remove unused code

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import styles from './Header.module.css';
 import BrainLogo from '../assets/BrainLogo';
 
-type HeaderProps = {
-  className?: string;
-};
-
 export default function Header(): JSX.Element {
   return (
     <header className={styles.header}>
