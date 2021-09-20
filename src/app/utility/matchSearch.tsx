d4e7fd799b9ec0c5867929e3d9f33d0d891d7479
dimas1976 <kogan.dmitriy@gmail.com>
2021-09-20 17:05:23 +0200

    ♻️ Refactor matchSearch function

--- src/app/utility/matchSearch.tsx
+++ src/app/utility/matchSearch.tsx
@@ -4,7 +4,7 @@ export function matchSearch(doctor: Doctor, searchValue: string): boolean {
   const concatedString =
     `${doctor.name}${doctor.surename}${doctor.adress}${doctor.city}`.toLowerCase();
   const searchValueLowerCase = searchValue.toLowerCase();
-  const regExpObj = new RegExp(`${searchValueLowerCase}*`, 'g');
+  const regExpObj = new RegExp(`${searchValueLowerCase}`, 'g');
   const result = concatedString.match(regExpObj);
   return result ? true : false;
 }
