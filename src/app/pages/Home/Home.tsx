968d9f52c9de853458e1a3f6c4b14fa5fb300606
dimas1976 <kogan.dmitriy@gmail.com>
2021-09-20 17:04:10 +0200

    ✨ Implement search by entering the page

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
 import Searchbar from '../../components/Searchbar/Searchbar';
 import Navigation from '../../components/navigation/Navigation';
@@ -6,49 +6,19 @@ import styles from './Home.module.css';
 import type { Doctor } from '../../lib/types';
 import DoctorCard from '../../components/DoctorCard/DoctorCard';
 import { matchSearch } from '../../utility/matchSearch';
-import { useHistory, useParams } from 'react-router';
+import { useHistory } from 'react-router';
 import { useLocation } from 'react-router-dom';
+import useLocalStorage from '../../hooks/useLocalStorage';
 
 export default function Home(): JSX.Element {
-  const [searchValue, setSearchValue] = useState('');
-  const [doctors, setDoctors] = useState<Doctor[]>([]);
-  const [errorMessage, setErrorMessage] = useState('');
+  const query = new URLSearchParams(useLocation().search);
+  const searchQuery = query.get('q');
+  const [searchValue, setSearchValue] = useState(searchQuery || '');
   const history = useHistory();
-  const location = useLocation();
-
-  // useEffect(() => {
-  //   setDoctors(doctors);
-  //   return () => {
-  //     document.removeEventListener('refetch');
-  //   };
-  // }, []);
-
-  // console.log('Location  ', location.state);
-  // history.listen((location, action) => {
-  //   if (action === 'PUSH') {
-  //     console.log(location);
-  //     //   history.replace(location.pathname, { fromProfilePage: true });
-  //     //   history.push(location.search);
-  //   }
-  // });
-
-  document.addEventListener('refetch', () => {
-    searchForDoctors(location.search);
-  });
+  const [doctors] = useLocalStorage<Doctor[]>('doctors', []);
 
   function searchForDoctors() {
-    history.push(`/search?q=${searchValue}`);
-
-    setErrorMessage('');
-    const item = localStorage.getItem('doctors');
-    const result: Doctor[] = item ? JSON.parse(item) : [];
-    const filteredResult = result.filter((element) =>
-      matchSearch(element, searchValue)
-    );
-    if (filteredResult.length === 0) {
-      setErrorMessage('Es gibt keine Ärzte unter diesem Suchbegriff');
-    }
-    setDoctors(filteredResult);
+    history.replace(`?q=${searchValue}`);
   }
 
   return (
@@ -62,24 +32,26 @@ export default function Home(): JSX.Element {
         onSubmit={searchForDoctors}
       />
       <section className={styles.page__section}>
-        {errorMessage ? <h3>{errorMessage}</h3> : ''}
-        {doctors.map((doctor) => {
-          return (
-            <DoctorCard
-              className={styles.card}
-              id={doctor.id}
-              key={doctor.id}
-              imgPath={doctor.image}
-              title={doctor.title}
-              name={doctor.name}
-              surename={doctor.surename}
-              adress={doctor.adress}
-              plz={doctor.plz}
-              city={doctor.city}
-              isShowButton={true}
-            />
-          );
-        })}
+        {searchValue &&
+          doctors
+            .filter((doctor) => matchSearch(doctor, searchValue))
+            .map((doctor) => {
+              return (
+                <DoctorCard
+                  className={styles.card}
+                  id={doctor.id}
+                  key={doctor.id}
+                  imgPath={doctor.image}
+                  title={doctor.title}
+                  name={doctor.name}
+                  surename={doctor.surename}
+                  adress={doctor.adress}
+                  plz={doctor.plz}
+                  city={doctor.city}
+                  isShowButton={true}
+                />
+              );
+            })}
       </section>
       <Navigation className={styles.page__navigation} activeLink="home" />
     </div>
