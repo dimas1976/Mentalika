6c72edfa7bd5a43daaf6d1e3b7d928236612989c
dimas1976 <kogan.dmitriy@gmail.com>
2021-09-26 13:56:52 +0200

    ✨ Add new route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AppointmentsOverview from './pages/AppointmentsOverview/AppointmentsOverview';
 import DoctorProfile from './pages/DoctorProfile/DoctorProfile';
 import Home from './pages/Home/Home';
 
@@ -9,6 +10,9 @@ function App(): JSX.Element {
       <Switch>
         <Route path="/doctor/:id">
           <DoctorProfile />
+        </Route>
+        <Route path="/overview">
+          <AppointmentsOverview />
         </Route>
         <Route path="/">
           <Home />
