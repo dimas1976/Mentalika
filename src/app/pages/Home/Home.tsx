3c42d9db100b1d44ea6c73d1edb116791324f714
dimas1976 <kogan.dmitriy@gmail.com>
2021-09-28 16:24:47 +0200

    ✨ Add initial value to local storage

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -12,15 +12,51 @@ import useLocalStorage from '../../hooks/useLocalStorage';
 import { useDebounce } from 'use-debounce';
 
 export default function Home(): JSX.Element {
+  const mockData = [
+    {
+      id: '1',
+      image: './src/app/components/assets/images/sabine.jpg',
+      title: 'Dipl.-Psych.',
+      name: 'Sabine',
+      surename: 'Wery von Limont',
+      bio: 'Hallo, mein Name ist Wery von Limont. Ich bin Diplom-Psychologin, Systemische Therapeutin und Heilpraktikerin für Psychotherapie. In meiner Privatpraxis arbeite ich mit klinischer Hypnose und lösungsorientierten Techniken',
+      adress: 'Max-Brauer-Alee 52',
+      plz: 22765,
+      city: 'Hamburg',
+    },
+    {
+      id: '2',
+      image: './src/app/components/assets/images/marc.jpg',
+      title: 'Dipl.-Psych.',
+      name: 'Marc',
+      surename: 'Almond',
+      bio: 'Gerne unterstütze ich Sie dabei, den passenden Ansatz für Ihr persönliches Anliegen zu finden. Dabei orientiere ich mich an den neuesten wissenschaftlichen Erkenntnissen der Psychotherapieforschung. Ich arbeite beziehungsorientiert und emotionsfokussiert und integriere die modernen und bewährten Verfahrensweisen der dritten Welle der Verhaltenstherapie, wie beispielsweise achtsamkeitsbasierte Ansätze.',
+      adress: 'Kastanienalee 18',
+      plz: 10715,
+      city: 'Berlin',
+    },
+    {
+      id: '3',
+      image: './src/app/components/assets/images/wilhelm.jpg',
+      title: 'Facharzt für Psychiatrie',
+      name: 'Wilhelm-Georg',
+      surename: 'Brokopf',
+      bio: '',
+      adress: 'Breite Str. 141-143',
+      plz: 22765,
+      city: 'Hamburg',
+    },
+  ];
   const query = new URLSearchParams(useLocation().search);
   const searchQuery = query.get('q');
   const [searchValue, setSearchValue] = useState(searchQuery || '');
   const history = useHistory();
-  const [doctors] = useLocalStorage<Doctor[]>('doctors', []);
+  const [doctors, setDoctors] = useLocalStorage<Doctor[]>('doctors', []);
   const [debouncedSearchValue] = useDebounce(searchValue, 500);
 
   useEffect(() => {
     if (searchValue === '') {
+      setDoctors(mockData);
       history.replace(``);
     } else {
       history.replace(`?q=${debouncedSearchValue}`);
